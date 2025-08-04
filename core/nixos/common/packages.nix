b5aaaa9c5b76bf6a867b6a1ee45e89a09af98b7d
J <204088010+Vidariondr@users.noreply.github.com>
2025-08-04 18:33:12 +0100

    feat(packages): add wiregruard-tools

--- core/nixos/common/packages.nix
+++ core/nixos/common/packages.nix
@@ -1,12 +1,12 @@
-{ config, pkgs, ... }:
-{
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     wget # Tool for downloading files using HTTP, HTTPS, and FTP
     ncdu # CLI file manager
     exfatprogs # exFAT utilities. Needed for formatting drives
-    edk2-uefi-shell # Tool used for secureboot with dualboot 
+    edk2-uefi-shell # Tool used for secureboot with dualboot
     btop # Task manager
-    nvtopPackages.amd # GPU utilization 
+    nvtopPackages.amd # GPU utilization
     ffmpeg # Video framework
+    wireguard-tools # Tools for the WireGuard secure network tunnel
   ];
 }
