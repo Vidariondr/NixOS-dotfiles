8d1c452acebcca106770168da6ae57e4128cab99
J <204088010+Vidariondr@users.noreply.github.com>
2025-09-19 21:29:22 +0200

    fix(dns): remove nameserver

--- core/nixos/common/networking-server-domain.nix
+++ core/nixos/common/networking-server-domain.nix
@@ -1,8 +1,5 @@
-{ config, ... }:
-{
-  networking.networkmanager.insertNameservers = [ "192.168.0.15" ];
-  networking.extraHosts = 
-    ''
-      192.168.0.15 homepage
-    '';
+{config, ...}: {
+  networking.extraHosts = ''
+    192.168.0.15 homepage
+  '';
 }
