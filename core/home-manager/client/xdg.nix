faa0411ac3c2fcba54c80f20df4aadccde0897a5
J <204088010+Vidariondr@users.noreply.github.com>
2025-10-14 13:03:30 +0200

    remove mime config

--- core/home-manager/client/xdg.nix
+++ core/home-manager/client/xdg.nix
@@ -1,27 +1,13 @@
-{ config, ... }:
-{
-  # Setting the XDG 
+{config, ...}: {
+  # Setting the XDG
   xdg = {
     enable = true;
- 
+
     # Setting user directories like Downloads, Music, etc...
     userDirs = {
-     enable = true;
-     createDirectories = true;
-     download = "${config.home.homeDirectory}/Downloads";
-    };
-
-    # Enable default apps management
-    mime.enable = true;
-    mimeApps = {
       enable = true;
-      defaultApplications = {
-        "application/pdf" = [ "floorp.desktop" ];
-        "text/plain" = [ "org.xfce.mousepad.desktop" ];
-        "model/stl"= [ "org.freecadweb.FreeCAD.desktop" ];
-        "image/jpeg " =  [ "org.nomacs.ImageLounge.desktop" ];
-        "image/png " =  [ "org.nomacs.ImageLounge.desktop" ];
-      };
+      createDirectories = true;
+      download = "${config.home.homeDirectory}/Downloads";
     };
   };
 }
