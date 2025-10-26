78e85c7de20484914f1281aabcc0773dc443460d
J <204088010+Vidariondr@users.noreply.github.com>
2025-10-26 21:17:23 +0100

    server(kopia): add timer

--- nixos/nixos-server/default.nix
+++ nixos/nixos-server/default.nix
@@ -1,9 +1,29 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ../../core/nixos/common
     ../../optional/nixos/docker
   ];
+
+  systemd.timers."kopia-backup" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "*-*-* 03:00:00";
+      Persistent = true;
+      Unit = "kopia-backup.service";
+    };
+  };
+
+  systemd.services."kopia-backup" = {
+    script = ''
+      ${pkgs.kopia}/bin/kopia snapshot create --all
+      ${pkgs.kopia}/bin/kopia snapshot verify --verify-files-percent=1 --file-parallelism=10 --parallel=10
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
 
   # Define hostname
   networking.hostName = "nixos_server";
