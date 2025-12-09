4aa1717baf14e0e866e72be1ce42a0caac7cec72
204088010+Vidariondr@users.noreply.github.com <Jakub>
2025-12-09 22:34:57 +0100

    change swaync config

--- core/home-manager/client/swaync.nix
+++ core/home-manager/client/swaync.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   home.packages = with pkgs; [
     swaynotificationcenter
   ];
@@ -7,66 +6,69 @@
   services.swaync = {
     enable = true;
     settings = {
-        "\$schema" = "${pkgs.swaynotificationcenter}/etc/xdg/swaync/configSchema.json";
-        positionX = "right";
-        positionY = "top";
-        layer = "overlay";
-        control-center-layer = "top";
-        layer-shell = true;
-        cssPriority = "application";
+      "\$schema" = "${pkgs.swaynotificationcenter}/etc/xdg/swaync/configSchema.json";
+      ignore-gtk-theme = true;
+      positionX = "right";
+      positionY = "bottom";
+      layer = "overlay";
+      layer-shell = true;
+      cssPriority = "user";
+      control-center-layer = "overlay";
+      control-center-positionX = "right";
+      control-center-positionY = "top";
 
-        control-center-width = 350;
-        control-center-height = 695;
-        control-center-margin-top = 1;
-        control-center-margin-bottom = 2;
-        control-center-margin-right = 1;
-        control-center-margin-left = 0;
+      control-center-width = 350;
+      control-center-height = 695;
+      control-center-margin-top = 1;
+      control-center-margin-bottom = 2;
+      control-center-margin-right = 1;
+      control-center-margin-left = 0;
 
-        notification-2fa-action = true;
-        notification-inline-replies = true;
-        notification-window-width = 300;
-        notification-icon-size = 50;
-        notification-body-image-height = 140;
-        notification-body-image-width = 180;
+      notification-2fa-action = true;
+      notification-inline-replies = true;
+      notification-window-width = 300;
+      notification-icon-size = 50;
+      notification-body-image-height = 140;
+      notification-body-image-width = 180;
 
-        timeout = 6;
-        timout-low = 3;
-        timout-critical = 0;
-        fit-to-screen = false;
-        keyboard-shortucts = false;
-        image-visibility = "when available";
-        transition-time = 200;
-        hide-on-clear = false;
-        hide-on-action = true;
-        script-fail-notify = true;
-        widgets = [
-          "title"
-          "notifications"
-          "mpris"
-          "volume"
-        ];
+      timeout = 6;
+      timout-low = 3;
+      timout-critical = 0;
+      fit-to-screen = false;
+      keyboard-shortucts = false;
+      image-visibility = "when available";
+      transition-time = 200;
+      hide-on-clear = true;
+      hide-on-action = true;
+      script-fail-notify = true;
+      widgets = [
+        "title"
+        "notifications"
+        "mpris"
+        "volume"
+      ];
 
-        widget-config = {
-          title = {
-            text = "Notifications";
-            clear-all-button = true;
-            button-text = "Clear";
-          };
-          dnd = {
-            text = " ";
-          };
-          label = {
-            max-lines = 2;
-            text = "Notifications";
-          };
-          mpris = {
-            image-size = 60;
-            image-radius = 10;
-          };
-          volume = {
-            label = "󰕾 ";
-          };
+      widget-config = {
+        title = {
+          text = "Notifications";
+          clear-all-button = true;
+          button-text = "Clear";
         };
+        dnd = {
+          text = " ";
+        };
+        label = {
+          max-lines = 2;
+          text = "Notifications";
+        };
+        mpris = {
+          image-size = 60;
+          image-radius = 10;
+        };
+        volume = {
+          label = "󰕾 ";
+        };
+      };
     };
   };
 }
