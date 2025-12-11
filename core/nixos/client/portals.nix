21b5aeaee26fced221ccbe61599dc46acf2eec68
204088010+Vidariondr@users.noreply.github.com <Jakub>
2025-12-11 20:42:04 +0100

    fix screensharing

--- core/nixos/client/portals.nix
+++ core/nixos/client/portals.nix
@@ -1,15 +1,18 @@
-{ config, pkgs, ... }:
 {
-  services.dbus.packages = [ pkgs.xdg-desktop-portal pkgs.xdg-desktop-portal-gtk ];
+  config,
+  pkgs,
+  ...
+}: {
+  services.dbus.packages = [pkgs.xdg-desktop-portal pkgs.xdg-desktop-portal-gtk];
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
   xdg.portal.wlr = {
     enable = true;
     settings = {
       screencast = {
         output_name = "DP-1";
         max_fps = 30;
-        chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+        chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Monitor: %o' -or";
         chooser_type = "simple";
       };
     };
