bcdabb6059f83e3f1974c6781cafe87c15c91157
Vidariondr <204088010+Vidariondr@users.noreply.github.com>
2026-06-22 09:13:59 +0200

    remove unnecessary

--- core/home-manager/client/home-manager-options.nix
+++ core/home-manager/client/home-manager-options.nix
@@ -8,8 +8,6 @@
     BROWSER = "floorp"; # Setting browser
     TERMINAL = "kitty"; # Setting terminal
 
-    XDG_RUNTIME_DIR = "/run/user/$(id -u)"; # Required for thunar showing default apps
-
     GDK_BACKEND = "wayland"; # Setting the GDK backend
 
     GTK_THEME = "Dracula"; # Setting the GTK theme
